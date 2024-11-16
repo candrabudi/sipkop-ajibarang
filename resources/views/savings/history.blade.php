39f69386779097d4d8b8be70a189e807cc3d9135
candrabudi <bagus.candrabudi@gmail.com>
2024-11-16 10:38:56 +0700

    fix

--- resources/views/savings/history.blade.php
+++ resources/views/savings/history.blade.php
@@ -39,7 +39,7 @@
                             <tr data-tw-merge="" class="intro-x">
                                 <td data-tw-merge=""
                                     class="px-5 py-3 border-b dark:border-darkmode-300 box rounded-l-none rounded-r-none border-x-0 shadow-[5px_3px_5px_#00000005] first:rounded-l-[0.6rem] first:border-l last:rounded-r-[0.6rem] last:border-r dark:bg-darkmode-600">
-                                    {{ $hs['amount'] }}
+                                    Rp {{ number_format($hs['amount'], 0, ',', '.') }}
                                 </td>
                                 <td data-tw-merge=""
                                     class="px-5 py-3 border-b dark:border-darkmode-300 box rounded-l-none rounded-r-none border-x-0 shadow-[5px_3px_5px_#00000005] first:rounded-l-[0.6rem] first:border-l last:rounded-r-[0.6rem] last:border-r dark:bg-darkmode-600">
